3fa4d1689fef3eeb4b1e90200aeb33f06236e998
Dafydd Lloyd <dlloyd@spartaglobal.com>
2024-05-31 16:19:09 +0100

    Added mongo setup bash

---
+++ mongo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install -y gnupg wget apt-transport-https ca-certificates software-properties-common
+wget -qO- https://pgp.mongodb.com/server-7.0.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-server-7.0.gpg > /dev/null
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | sudo tee -a /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl enable --now mongod
+sudo sed -i "s/^bindIP.*/bindIP: 0.0.0.0" /etc/mongod.conf
+sudo systemctl restart mongod.service

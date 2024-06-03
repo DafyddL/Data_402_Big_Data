bf68ed9268a3ddbb85dfe7af91090032df4a080b
Dafydd Lloyd <DLloyd@spartaglobal.com>
2024-06-03 10:51:45 +0100

    added -y

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install mysql-server
+sudo apt install -y mysql-server
 sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl restart mysql

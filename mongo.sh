ea65bc92bd0bf144d5506a592e5385b9b0edbdb6
Dafydd Lloyd <DLloyd@spartaglobal.com>
2024-06-03 11:08:45 +0100

    added apt update to start of mongo.sh script

--- mongo.sh
+++ mongo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt update -y
 sudo apt install -y gnupg wget apt-transport-https ca-certificates software-properties-common
 wget -qO- https://pgp.mongodb.com/server-7.0.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-server-7.0.gpg > /dev/null
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | sudo tee -a /etc/apt/sources.list.d/mongodb-org-7.0.list
